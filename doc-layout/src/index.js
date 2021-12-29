cf812ce63e0ad4046f4ebb4d56ab465cf0a39bf9
m4dz <471486+undefined@users.noreply.github.com>
2021-12-29 15:55:54 +0000

    Improve doc look n feel

--- doc-layout/src/index.js
+++ doc-layout/src/index.js
@@ -1,6 +1,8 @@
 import '@divriots/dockit-core/style.css';
-import 'open-props/fonts.min.css';
+
+import 'open-props/open-props.min.css';
 import 'open-props/normalize.min.css';
-import './extra.css';
+
+import './doc-layout.css';
 
 import './open-props.svg.js';
